0393b5d7e266cbde469e551131daa94b87bb56b6
guihRovetta <guilherme020213@hotmail.com>
2020-09-14 01:15:52 -0300

    🎉 begin strapi api project

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7b25ac98ab797c1faf633ec423342596'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b0dff4cc-fced-4422-b225-409102525895'
+};
